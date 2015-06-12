af7a60fb0c6fdf3e4c45db2a701a4aefd0b022b6
Slawomir Borowiec <przodownik@tlen.pl>
2015-06-12 14:59:58 +0200

    simple function unary & function test

--- src/test/java/pl/java/scalatech/lambda/ActionTest.java
+++ src/test/java/pl/java/scalatech/lambda/ActionTest.java
@@ -1,5 +1,10 @@
 package pl.java.scalatech.lambda;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.junit.Test;
@@ -16,4 +21,13 @@ public class ActionTest {
         a.print();
     }
 
+    @Test
+    public void test2() {
+        UnaryOperator<Integer> operator = v -> v * 2;
+        Function<Integer, Integer> function = v -> v * 2;
+        assertThat(operator.apply(2)).isEqualTo(function.apply(2));
+        Runnable r = () -> log.info("run .....");
+        Thread t = new Thread(r);
+        t.start();
+    }
 }
