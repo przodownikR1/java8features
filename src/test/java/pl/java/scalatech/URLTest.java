8a2507743aa83e92463796fca6689b6f82141fee
Slawomir Borowiec <przodownik@tlen.pl>
2015-07-07 12:41:00 +0200

    URL / guava read / write

---
+++ src/test/java/pl/java/scalatech/URLTest.java
@@ -0,0 +1,50 @@
+package pl.java.scalatech;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.common.base.Charsets;
+
+@Slf4j
+public class URLTest {
+
+    private static File file;
+    private URI fileLocation;
+
+    @BeforeClass
+    public static void init() {
+        file = new File("src/test/resources/strings.txt");
+    }
+
+    @Test
+    public void shouldFilelocationExists() {
+        log.info("{}", fileLocation);
+    }
+
+    @Test
+    public void shouldURLConnect() throws IOException {
+        URL url = new URL("http://przewidywalna-java.blogspot.com/");
+        StringBuilder fileContent;
+        try (InputStream in = url.openStream(); BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));) {
+            fileContent = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                fileContent.append(line);
+                line = bufferedReader.readLine();
+            }
+        }
+        assertThat(fileContent.toString().contains("przewidywalna-java.blogspot.com/search/label")).isTrue();
+    }
+}
