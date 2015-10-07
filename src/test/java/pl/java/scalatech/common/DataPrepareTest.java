68f898e7897683bf6c6e328d54f6c80ac055853f
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-07 11:32:10 +0200

    exception test

--- src/test/java/pl/java/scalatech/common/DataPrepareTest.java
+++ src/test/java/pl/java/scalatech/common/DataPrepareTest.java
@@ -2,8 +2,10 @@ package pl.java.scalatech.common;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
+import org.assertj.core.util.Maps;
 import org.junit.Before;
 
 import pl.java.scalatech.pojo.Person;
@@ -13,6 +15,7 @@ import com.google.common.collect.Lists;
 public abstract class DataPrepareTest {
     protected List<Person> persons;
     protected List<Integer> values = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8);
+    protected Map<Integer,Person> maps = Maps.newHashMap();
     protected Random random = new Random();
     protected List<String> names = Lists.newArrayList("slawek", "bak", "agnieszka", "slawek", "bak", "bak", "tyson");
 
@@ -20,5 +23,9 @@ public abstract class DataPrepareTest {
     public void init() {
         persons = Lists.newArrayList(new Person("slawek", "przodownik", new BigDecimal(12)), new Person("slawek", "przodownik2", new BigDecimal(23)),
                 new Person("aga", "poka", new BigDecimal(6)), new Person("kalina", "bak", new BigDecimal(13)));
+        maps.put(1, new Person("slawek", "przodownik", new BigDecimal(12)));
+        maps.put(2,  new Person("slawek", "przodownik2", new BigDecimal(23)));
+        maps.put(3, new Person("aga", "poka", new BigDecimal(6)));
+        maps.put(4, new Person("kalina", "bak", new BigDecimal(13)));
     }
 }
