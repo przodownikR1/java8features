ec6951ddfd9f7a558dc1660d50f5498702d07132
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-28 14:46:24 +0100

    parallel vs seq processing

---
+++ src/test/java/pl/java/scalatech/stream/ParallelTest.java
@@ -0,0 +1,26 @@
+package pl.java.scalatech.stream;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class ParallelTest {
+
+    List<String> strs = newArrayList("slawek","aga","kalina","tomek");
+    Consumer<String> c = x-> log.info(" item : {} , thread : {}",x,Thread.currentThread());
+    @Test
+    public void seqTest(){
+        strs.stream().forEach(c);
+    }
+    
+    @Test
+    public void parallelTest(){
+        strs.stream().parallel().forEach(c);
+    }
+    
+}
