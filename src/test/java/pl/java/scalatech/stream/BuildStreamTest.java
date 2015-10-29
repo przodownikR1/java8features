c39694ba27eaea070d5df57f3fafb8a795eee056
Slawomir Borowiec <przodownik@tlen.pl>
2015-10-29 16:29:48 +0100

    stream gen

---
+++ src/test/java/pl/java/scalatech/stream/BuildStreamTest.java
@@ -0,0 +1,17 @@
+package pl.java.scalatech.stream;
+
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class BuildStreamTest {
+
+    @Test
+    public void shouldBuildStream(){
+        IntStream ones = IntStream.generate(() -> 1);
+        ones.limit(10).forEach(l->log.info("{}",l));
+    }
+    
+}
